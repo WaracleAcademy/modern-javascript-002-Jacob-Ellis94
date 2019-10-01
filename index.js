52167af23413f722a1fe760d2e130611289f4e1d
Jacob-Ellis94 <50924161+Jacob-Ellis94@users.noreply.github.com>
2019-10-01 18:08:50 +0100

    update of files

---
+++ index.js
@@ -0,0 +1,6 @@
+const = things['Donald Trump', 'Xi Jinping', 'Vladimir Putin', 'Jean Claude Junker'];
+const mainView = document.querySelector('#primaryView');
+
+mainView.innerHTML = ' <
+  h1 > DIFFERENT < /h1>
+';
