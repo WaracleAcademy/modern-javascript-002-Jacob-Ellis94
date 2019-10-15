6dcc5942b1a1f09a35d9fdb97e2024ba57300652
Jacob-Ellis94 <50924161+Jacob-Ellis94@users.noreply.github.com>
2019-10-15 18:38:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-import {render} from './renderer.js';
+import {
+  render
+} from './renderer.js';
+import * as data from './thing.json';
 
+function getThingTemplate(thing) {
+  return `<li> ${thing.Name} - ${thing.description}</li>`;
+};
 
+console.log(data.Leaders);
+function sortMyStuff(first, second){
+  if(first.Name > second.Name){
+  return 1;
+}
+else if (first.Name < second.Name){
+  return -1;
+}
+else {
+  return 0;
+}
+}
+const things = data.Leaders.sort(sortMyStuff);
+let content='<ul>';
+
+for (let i=0; i< things.length; i++){
+  console.log(things[i]);
+content += getThingTemplate(things[i]);
+
+};
+
+render(content);
 
 const mainView = document.querySelector('#primaryView');
 
-render(`
-<h2> CHANGES HERE WHY NOT WORKING </h2>
-`);
-
-
-console.log('BLAHBLAHBLAHBLAH');
-console.log('CHANGES');
+//render(`
+//<h2> TESTING TESTING 123 123</h2>
+//`);
